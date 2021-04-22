3c552578fb7de85b6912a8be722edc824c523702
miksmit <mikkosmith@gmail.com>
2021-04-22 13:02:16 -0600

    ProfessorController finalized.

--- LMS/Controllers/ProfessorController.cs
+++ LMS/Controllers/ProfessorController.cs
@@ -106,6 +106,9 @@ namespace LMS.Controllers
         /// <returns>The JSON array</returns>
         public IActionResult GetStudentsInClass(string subject, int num, string season, int year)
         {
+            // Query Enrolled students
+            // Join students to get student info
+            // Join class, course, and semester to identify this class based on input parameters.
             var query =
                 from e in db.Enrolled
                 join s in db.Student on e.SId equals s.SId
@@ -145,7 +148,8 @@ namespace LMS.Controllers
         /// <returns>The JSON array</returns>
         public IActionResult GetAssignmentsInCategory(string subject, int num, string season, int year, string category)
         {
-            
+            // Query Assignments in this category
+            // Joins necessary because Assignments belong to AssignmentCategory belong to Class identified by Semester and Course.
             var query =
                 from cl in db.Class
                 join cr in db.Course on cl.CourseId equals cr.CourseId
@@ -179,6 +183,8 @@ namespace LMS.Controllers
         /// <returns>The JSON array</returns>
         public IActionResult GetAssignmentCategories(string subject, int num, string season, int year)
         {
+            // Query the assignment Categories for this class.
+            // AssignmentCategory belongs to a class identified by Semester (season/year) and Course (subject/num).
             var query =
                 from cl in db.Class
                 join cr in db.Course on cl.CourseId equals cr.CourseId
@@ -207,6 +213,7 @@ namespace LMS.Controllers
         ///	false if an assignment category with the same name already exists in the same class.</returns>
         public IActionResult CreateAssignmentCategory(string subject, int num, string season, int year, string category, int catweight)
         {
+            // Query table to check if assignment category with the same name already exists in the same class.
             var query =
                 from cl in db.Class
                 join cr in db.Course on cl.CourseId equals cr.CourseId
@@ -215,11 +222,14 @@ namespace LMS.Controllers
                 where cr.DeptAbbr == subject && cr.Number == num && sm.Season == season && sm.Year == year && ac.Name == category
                 select ac.Name;
 
+            // If the above query found any results, then there already exists an AssignmentCategory
+            // with the same name in the same class.
             if (query.ToArray().Length > 0)
             {
                 return Json(new { success = false });
             }
 
+            // Grab the class ID for the class specified by the input parameters.
             var queryForClassID =
                 from cl in db.Class
                 join cr in db.Course on cl.CourseId equals cr.CourseId
@@ -229,14 +239,27 @@ namespace LMS.Controllers
           
             uint classID = queryForClassID.First();
 
+            // Create the new Assignment category with the specified name/weight and the ClassID from above.
             AssignmentCategory newAssnCat = new AssignmentCategory();
             newAssnCat.Name = category;
             newAssnCat.Weight = (uint)catweight;
             newAssnCat.ClassId = classID;
-            db.AssignmentCategory.Add(newAssnCat);
-            db.SaveChanges();
 
-            return Json(new { success = true });
+            // only return true if the addition saves successfully.
+            try
+            {
+                db.AssignmentCategory.Add(newAssnCat);
+                db.SaveChanges();
+
+                return Json(new { success = true });
+            }
+            catch(Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+            }
+
+            // return false if the try failed. Changes were not saved successfully to db.
+            return Json(new { success = false });
         }
 
         /// <summary>
@@ -255,6 +278,7 @@ namespace LMS.Controllers
         /// false if an assignment with the same name already exists in the same assignment category.</returns>
         public IActionResult CreateAssignment(string subject, int num, string season, int year, string category, string asgname, int asgpoints, DateTime asgdue, string asgcontents)
         {
+            // Query to check if an assignment with the same name already exists in the same assignment category for this class
             var query =
                 from cl in db.Class
                 join cr in db.Course on cl.CourseId equals cr.CourseId
@@ -264,11 +288,13 @@ namespace LMS.Controllers
                 where cr.DeptAbbr == subject && cr.Number == num && sm.Season == season && sm.Year == year && ac.Name == category && ag.Name == asgname
                 select ac.Name;
 
+            // if the above query found a duplicate assignment, return false.
             if(query.ToArray().Length > 0)
             {
                 return Json(new { success = false });
             }
 
+            // query for the correct Assignment Category ID with the given class input parameters
             var queryForAssnCatID =
                 from cl in db.Class
                 join cr in db.Course on cl.CourseId equals cr.CourseId
@@ -278,17 +304,29 @@ namespace LMS.Controllers
                 select ac.AssnCategoryId;
 
             uint categoryID = queryForAssnCatID.First();
-
+            
+            // Create the new assignment with the name/maxpoints/dueDate given as input parameters.
+            // CategoryID from above query.
             Assignment newAssn = new Assignment();
             newAssn.Name = asgname;
             newAssn.MaxPoints = (uint)asgpoints;
             newAssn.DueDate = asgdue;
             newAssn.Contents = asgcontents;
             newAssn.AssnCategoryId = categoryID;
-            db.Assignment.Add(newAssn);
-            db.SaveChanges();
 
-            return Json(new { success = true });
+            // only return true if the addition saves successfully.
+            try
+            {
+                db.Assignment.Add(newAssn);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+            }
+            // return false if the try failed.Changes were not saved successfully to db.
+            return Json(new { success = false });
         }
 
 
@@ -311,6 +349,10 @@ namespace LMS.Controllers
         /// <returns>The JSON array</returns>
         public IActionResult GetSubmissionsToAssignment(string subject, int num, string season, int year, string category, string asgname)
         {
+            // Query Submissions
+            // Join Students to get student info for submission
+            // Join Assignment, AssignmentCategory, Class, Semester, Course
+            // to identify class and Assignment given by input parameters
             var query =
                 from cl in db.Class
                 join cr in db.Course on cl.CourseId equals cr.CourseId
@@ -319,7 +361,7 @@ namespace LMS.Controllers
                 join sb in db.Submission on ag.AssnId equals sb.AssnId 
                 join sm in db.Semester on cl.SemesterId equals sm.SemesterId
                 join st in db.Student on sb.SId equals st.SId
-                //where cr.DeptAbbr == subject & cr.Number == num & sm.Season == season & sm.Year == year & ac.Name == category & ag.Name == asgname
+                where cr.DeptAbbr == subject & cr.Number == num & sm.Season == season & sm.Year == year & ac.Name == category & ag.Name == asgname
                 select new
                 {
                     fname = st.FName,
@@ -347,6 +389,7 @@ namespace LMS.Controllers
         /// <returns>A JSON object containing success = true/false</returns>
         public IActionResult GradeSubmission(string subject, int num, string season, int year, string category, string asgname, string uid, int score)
         {
+            // Query to select this submission specified by input parameters.
             var query =
                 from st in db.Student
                 join e in db.Enrolled on st.SId equals e.SId
@@ -359,16 +402,20 @@ namespace LMS.Controllers
                 where cr.DeptAbbr == subject && cr.Number == num && sm.Season == season && sm.Year == year && ac.Name == category && ag.Name == asgname && st.SId == uid
                 select sb;
 
+
+            // if this submission was not found, or if duplicate submissions are found, return false.
             if (query.ToArray().Length != 1 )
             {
                 return Json(new { success = false });
             }
 
+            // Update score to score given as method parameter
             foreach (Submission submission in query)
             {
                 submission.Score = (uint)score;
             }
 
+            // only return true if changes save successfully.
             try
             {
                 db.SaveChanges();
@@ -378,6 +425,8 @@ namespace LMS.Controllers
             {
                 System.Diagnostics.Debug.WriteLine("Error Grading assignment {" + asgname + "} with error: " + e.Message);
             }
+
+            // return false if the try failed. The changes were not saved successfully.
             return Json(new { success = false });
         }
 
@@ -395,6 +444,8 @@ namespace LMS.Controllers
         /// <returns>The JSON array</returns>
         public IActionResult GetMyClasses(string uid)
         {
+            // Query the db for classes taught by this professor.
+            // Join Course and Semester for output values.
             var query =
                 from cl in db.Class
                 join cr in db.Course on cl.CourseId equals cr.CourseId
